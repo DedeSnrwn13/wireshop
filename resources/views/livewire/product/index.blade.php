d9b64e296c11af9ddfd2dd8161099bde4cbdf063
DedeSnrwn13 <sunarwandede13@gmail.com>
2021-10-11 14:47:27 +0700

    Membuat Fungsi Delete Product

--- resources/views/livewire/product/index.blade.php
+++ resources/views/livewire/product/index.blade.php
@@ -17,7 +17,7 @@
                 </div>
 
                 <div class="card-body">
-                    
+
                     @if (session()->has('message'))
                         <div class="alert alert-success">
                             {{ session('message') }}
@@ -62,7 +62,7 @@
                                     <td>Rp {{ number_format($product->price,2,",",".") }}</td>
                                     <td>
                                         <button wire:click="editProduct({{ $product->id }})" class="btn btn-sm btn-info text-white">Edit</button>
-                                        <button class="btn btn-sm btn-danger">Delete</button>
+                                        <button wire:click="deleteProduct({{ $product->id }})" class="btn btn-sm btn-danger">Delete</button>
                                     </td>
                                 </tr>
                             @endforeach
