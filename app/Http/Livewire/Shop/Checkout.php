0ed6e8456e6a09f9b2b595223c4662ba8bb01f5c
DedeSnrwn13 <sunarwandede13@gmail.com>
2021-10-13 23:21:19 +0700

    Implementasi Payment Gateway Midtrans

--- app/Http/Livewire/Shop/Checkout.php
+++ app/Http/Livewire/Shop/Checkout.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Livewire\Shop;
 
+use Midtrans\Snap;
+use Midtrans\Config;
+use App\Facades\Cart;
 use Livewire\Component;
 
 class Checkout extends Component
@@ -14,6 +17,11 @@ class Checkout extends Component
     public $city;
     public $postal_code;
     public $formCheckout;
+    public $snapToken;
+
+    protected $listeners = [
+        'emptyCart' => 'emptyCartHandler'
+    ];
 
     public function mount()
     {
@@ -36,5 +44,51 @@ class Checkout extends Component
             'city' => 'required',
             'postal_code' => 'required|numeric',
         ]);
+
+        $cart = Cart::get()['products'];
+        $amount = array_sum(
+            array_column($cart, 'price')
+        );
+
+        $customerDetails = [
+            'first_name' => $this->first_name,
+            'last_name' => $this->last_name,
+            'email' => $this->email,
+            'phone' => $this->phone,
+            'address' => $this->address,
+            'city' => $this->city,
+            'postal_code' => $this->postal_code,
+        ];
+
+        $transactionDetails = [
+            'order_id' => uniqid(),
+            'gross_amount' => $amount,
+        ];
+
+        $payload = [
+            'transaction_details' => $transactionDetails,
+            'customer_details' => $customerDetails,
+        ];
+
+        $this->formCheckout = false;
+
+        // Set your Merchant Server Key
+        Config::$serverKey = config('services.midtrans.serverKey');
+        // Set to Development/Sandbox Environment (default). Set to true for Production Environment (accept real transaction).
+        Config::$isProduction = config('services.midtrans.isProduction');
+        // Set sanitization on (default)
+        Config::$isSanitized = config('services.midtrans.isSanitized');
+        // Set 3DS transaction for credit card to true
+        Config::$is3ds = config('services.midtrans.is3ds');
+
+        $snapToken = Snap::getSnapToken($payload);
+
+        $this->snapToken = $snapToken;
+    }
+
+    public function emptyCartHandler()
+    {
+        Cart::clear();
+        $this->emit('cartClear');
     }
 }
